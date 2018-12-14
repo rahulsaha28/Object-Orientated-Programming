c67ac5a8348cfc876ff5e85f0bae787a194b0b0c
Rahul Saha <saharahul039@gmail.com>
2018-12-14 09:02:47 +0600

    Add files via upload

---
+++ DataType/P2.py
@@ -0,0 +1,35 @@
+
+
+var_1 = 5;
+
+# print the memory address
+print(id(var_1));
+
+var_1 = 6
+
+# print the memory address
+print(id(var_1));
+
+
+'''
+If we see the output which is the memory address of the variable.
+if the output are not same then the variable change it memory address  
+'''
+
+
+var_2 = ['Rafid', 'Milon', 'Liton', 'Nakib'];
+# print the memory address
+print(id(var_2));
+
+var_2.append('Suzon');
+
+# print the memory address
+print(id(var_2));
+
+
+'''
+If we see the output which is the memory address of the variable.
+if the output are  same then the variable can't change it memory address  
+'''
+
+
