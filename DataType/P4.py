8728a8cc4f8d450f93c6b78905bb0ba48710db40
Rahul Saha <saharahul039@gmail.com>
2018-12-14 09:03:19 +0600

    Add files via upload

---
+++ DataType/P4.py
@@ -0,0 +1,13 @@
+'''
+    Working with method
+'''
+
+myNumericDigit = '12347';
+
+myNonNumericDigit = 'Md Kadir';
+
+# print true if string is digit
+print(myNumericDigit.isdigit());
+
+# print false if string are not digit
+print( myNonNumericDigit.isdigit() );
